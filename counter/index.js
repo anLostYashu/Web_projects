a89f795bbc0a36c18b42a0f3b3418547316c798d
unknown <theyashuuu37@gmail.com>
2024-01-16 19:02:58 +0530

    counter application

---
+++ counter/index.js
@@ -0,0 +1,16 @@
+const heading = document.getElementById("counter-head");
+const decrement = document.getElementById("dec-button");
+const reset = document.getElementById("res-button");
+const increment = document.getElementById("inc-button");
+
+decrement.onclick = function() {
+    heading.textContent = Number(heading.textContent) - 1;
+}
+
+reset.onclick = function() {
+    heading.textContent = 0;
+}
+
+increment.onclick = function() {
+    heading.textContent = Number(heading.textContent) + 1;
+}
