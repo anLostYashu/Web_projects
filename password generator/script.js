bb8fd59ac9ea53c4022346853cd6b79d6d6cd73c
unknown <theyashuuu37@gmail.com>
2024-01-19 18:46:19 +0530

    updated code

--- password generator/script.js
+++ password generator/script.js
@@ -4,31 +4,47 @@ const incSymbols = document.getElementById("symbols_box");
 const incNums = document.getElementById("numbers_box");
 const output = document.getElementById("output");
 const generate = document.getElementById("generate_button");
+const copy = document.getElementById("copyButton");
 
-function generatePassword(){
-    const lowercase = "abcdefghijklmnopqrstuvwxyz"
-    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    const symbols = "!@#$%^&*_"
-    const numbers = "0123456789"
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const symbols = "!@#$%^&*_";
+const numbers = "0123456789";
 
-    let allChars = [];
+let password = [];
 
-    if(incLower.checked){
-        allChars.push(lowercase);
-    }
-    if(incUpper.checked){
-        allChars.push(uppercase);
-    }
-    if(incSymbols.checked)
-    {
-        allChars.push(symbols);
-    }
-    if(incNums.checked){
-        allChars.push(numbers);
-    }
-    if(allChars === 0){
-        return 'select at least 1 character set.'
-    }
+generate.onclick = function () {
+  let allChars = [];
+  const size = 12;
 
-    console.log(allChars);
-}
+  if (incLower.checked) {
+    allChars.push(...lowercase);
+  }
+  if (incUpper.checked) {
+    allChars.push(...uppercase);
+  }
+  if (incSymbols.checked) {
+    allChars.push(...symbols);
+  }
+  if (incNums.checked) {
+    allChars.push(...numbers);
+  }
+  if (allChars === 0) {
+    return "select at least 1 character set.";
+  }
+
+  for (let i = 0; i < size; i++) {
+    let random = Math.floor(Math.random() * allChars.length);
+
+    password.push(allChars[random]);
+  }
+
+  password = password.join("");
+  output.textContent = password;
+
+  copy.style.display = "block";
+};
+
+copy.onclick = function () {
+  navigator.clipboard.writeText(password);
+};
