5294b45a6ea1d62e26cbe3788b9e13aa26bf79e6
unknown <theyashuuu37@gmail.com>
2024-01-21 20:32:02 +0530

    errors fixed: final code

--- password generator/script.js
+++ password generator/script.js
@@ -1,50 +1,59 @@
+//checkboxes
 const incLower = document.getElementById("lowercase_box");
 const incUpper = document.getElementById("uppercase_box");
 const incSymbols = document.getElementById("symbols_box");
 const incNums = document.getElementById("numbers_box");
-const output = document.getElementById("output");
-const generate = document.getElementById("generate_button");
-const copy = document.getElementById("copyButton");
 
+//output and buttons
+const outputField = document.getElementById("output");
+const generateButton = document.getElementById("generate_button");
+const copyButton = document.getElementById("copyButton");
+
+//character sets
 const lowercase = "abcdefghijklmnopqrstuvwxyz";
 const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const symbols = "!@#$%^&*_";
 const numbers = "0123456789";
 
+
+//global vars
 let password = [];
 
-generate.onclick = function () {
-  let allChars = [];
-  const size = 12;
+generateButton.onclick = function () {
+    password = [];
 
-  if (incLower.checked) {
-    allChars.push(...lowercase);
-  }
-  if (incUpper.checked) {
-    allChars.push(...uppercase);
-  }
-  if (incSymbols.checked) {
-    allChars.push(...symbols);
-  }
-  if (incNums.checked) {
-    allChars.push(...numbers);
-  }
-  if (allChars === 0) {
-    return "select at least 1 character set.";
-  }
+    let allowedChars = [];
+    const size = 12;
 
-  for (let i = 0; i < size; i++) {
-    let random = Math.floor(Math.random() * allChars.length);
+    if(incLower.checked){
+        allowedChars.push(...lowercase);
+    }
+    if (incUpper.checked) {
+        allowedChars.push(...uppercase);
+    }
+    if (incSymbols.checked) {
+        allowedChars.push(...symbols);
+    }
+    if (incNums.checked) {
+        allowedChars.push(...numbers);
+    }
+    if(allowedChars === 0) {
+        return 'no character sets selected!';
+    }
 
-    password.push(allChars[random]);
-  }
+    for(let i = 0; i < size; i++) {
+        const rand = Math.floor(Math.random() * allowedChars.length);
 
-  password = password.join("");
-  output.textContent = password;
+        password.push(allowedChars[rand]);
+    }
 
-  copy.style.display = "block";
+    password = password.join("");
+
+    outputField.textContent = password;
+    copyButton.style.display = "block";
 };
 
-copy.onclick = function () {
-  navigator.clipboard.writeText(password);
+copyButton.onclick = function () {
+    navigator.clipboard.writeText(password);
+    window.alert("Password has been copied!");
 };
