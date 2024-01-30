bed8d72a1627b8f10bd8bbf82be7da52cd6d6509
unknown <theyashuuu37@gmail.com>
2024-01-30 22:15:55 +0530

    html + css done

---
+++ rock paper scissors/script.js
@@ -0,0 +1,12 @@
+const options = ["rock", "paper", "scissors"];
+
+const displayPlayerMove = document.getElementById("span-player");
+const displayBotMove = document.getElementById("span-bot");
+const playerScore = document.getElementById("span-player-score");
+const botScore = document.getElementById("span-bot-score");
+
+function game(val) {
+
+    let playerMove = val;
+
+}
